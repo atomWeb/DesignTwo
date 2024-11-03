6aa95135ddc19423acdbb764c241db54c6392c73
nabusim <cristianjose@gmail.com>
2024-11-03 22:41:58 +0100

    Initial Commit

---
+++ DesignTwo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignTwo
+//
+//  Created by Cristian Jose Perez Guerra on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignTwo/DesignTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignTwoApp.swift
+//  DesignTwo
+//
+//  Created by Cristian Jose Perez Guerra on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
